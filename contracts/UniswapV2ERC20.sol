c9187a0cb6882976ee2de4b70253fdc9d2de73d0
jedinrk <jedi.nrk@gmail.com>
2023-02-10 17:16:11 +0400

    Updated the contracts

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -1,13 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity =0.6.12;
 
-import './interfaces/IUniswapV2ERC20.sol';
 import './libraries/SafeMath.sol';
 
-contract UniswapV2ERC20 is IUniswapV2ERC20 {
+contract UniswapV2ERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'Uniswap V2';
-    string public constant symbol = 'UNI-V2';
+    string public constant name = 'SushiSwap LP Token';
+    string public constant symbol = 'SLP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
