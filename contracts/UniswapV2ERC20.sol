e965d6431171a1218258df6894a42a6d607cb60b
jedinrk <jedi.nrk@gmail.com>
2023-02-10 17:17:01 +0400

    Updated LP token name and symbol

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -7,8 +7,8 @@ import './libraries/SafeMath.sol';
 contract UniswapV2ERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'SushiSwap LP Token';
-    string public constant symbol = 'SLP';
+    string public constant name = 'Murabah LP Token';
+    string public constant symbol = 'MRBLP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
